a58b509cc84b335c25d93ed5e3fbc04ed3f5b899
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-07-18 10:14:18 -0400

    Add files via upload

---
+++ ByteShortInt.java
@@ -0,0 +1,20 @@
+public class ByteShortInt {
+    public static void main(String[] args) {
+	
+	// Integer has a width of 32
+	int myMinValue = -2147483648;
+	int myMaxValue = -2147483647;
+
+	// Byte has a width of 4
+	byte minByteValue = -128;	
+	byte maxByteValue = 127;
+
+	// Short has a width of 16
+	short minShortValue = -32768;	
+	short maxShortValue = 32767;
+
+	// Long has a width of 64
+	long minLongValue = -9223372036854775808L;
+	long maxLongValue = 9223372036854775807L;
+	}
+}
